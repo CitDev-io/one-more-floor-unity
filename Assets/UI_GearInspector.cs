eb3f4db209336135c6f19ce241284fa036564878
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2022-04-27 23:25:49 -0400

    Inspectable Gear in Stat Window

---
+++ Assets/UI_GearInspector.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using System;
+using System.Collections.Generic;
+
+public class UI_GearInspector : MonoBehaviour
+{
+    GridGameManager _rc;
+    [SerializeField] TextMeshProUGUI ItemTitle;
+    [SerializeField] TextMeshProUGUI ItemDesc;
+    [SerializeField] TextMeshProUGUI ItemStats;
+    [SerializeField] List<GameObject> HideObjectsOnStart;
+
+    void Awake()
+    {
+        _rc = GameObject.FindObjectOfType<GridGameManager>();
+    }
+
+    void OnEnable() {
+        foreach(GameObject g in HideObjectsOnStart) {
+            g.SetActive(false);
+        }
+        ItemTitle.text = "";
+        ItemDesc.text = "";
+        ItemStats.text = "";
+    }
+
+    public void InspectInventoryAtSlot(string slot) {
+        ItemSlot affectedSlot = Enum.Parse<ItemSlot>(slot);
+        PlayerItem item = _rc.Board.Player.GetItemInInventorySlot(affectedSlot);
+        ItemTitle.text = item.Name.ToUpper();
+        ItemDesc.text = item.Description.ToUpper();
+        ItemStats.text = item.SummaryStats();
+    }
+}
